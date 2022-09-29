b747dc59c437e83be508215460038bf7e7219b62
Uno <463453@bsd48.org>
2022-09-29 13:30:03 -0700

    More random students and stuff

--- Main.cpp
+++ Main.cpp
@@ -16,9 +16,16 @@ void printList(Node* n) {
 
 int main() {
   // Make list 
-  Node* listHead = new Node(new Student((char*)"StudentOne", (char*)"Lastname", 5, 5));
-  listHead->setNext(new Node(new Student((char*)"StudentTwo", (char*)"Lastname", 3, 3)));
-  listHead->getNext()->setNext(new Node(new Student((char*)"StudentThree", (char*)"Lastname", 69, 69)));
+  Node* listHead = new Node(new Student((char*)"First", (char*)"Student", 123456, 5)); 
+  Node* n = listHead;
+  srand(time(NULL));
+  for (int i = 0; i < 9; i++) {
+    Node* nn = new Node(new Student((char*)"Firstname", (char*)"Lastname",
+				    (rand() % 900001) + 100000, (float)(rand() % 401 + 101) / 100.f)); 
+    n->setNext(nn);
+    n = nn;
+  }
+
   // Print the list 
   printList(listHead);
   return 0;
